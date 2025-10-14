46825604c1c17db57c4bcfda8e82302d017115a4
Jordan Carr <jccarr3@ncsu.edu>
2025-10-14 18:45:27 -0400

    power to pico test

--- blinky_personal.py
+++ blinky_personal.py
@@ -4,7 +4,9 @@ import time
 from machine import Pin
 
 led = Pin(25, Pin.OUT)
+v_out = Pin(28, Pin.OUT)
 
+v_out.value(1)  # power pico board
 while True:
     led.value(0)  # yellow LED on
     time.sleep_ms(1000)
