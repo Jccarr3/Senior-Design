975d43816632105ecd2503a730af6c2e35f78439
Jordan Carr <jccarr3@ncsu.edu>
2025-10-19 20:32:52 -0400

    PICO + Zumo Interaction V2

--- blinky_personal.py
+++ blinky_personal.py
@@ -4,12 +4,13 @@ import time
 from machine import Pin
 
 led = Pin(25, Pin.OUT)
-v_out = Pin(28, Pin.OUT)
-trigger = Pin(25, Pin.IN)
+trigger = Pin(28, Pin.IN, Pin.PULL_DOWN)
 
-v_out.value(1)  # power pico board
+flag = 0
+
 while True:
-    if(trigger.value() == 1):
+    if(trigger.value() == 1 or flag == 1):
+        flag = 1
         led.value(0)  # yellow LED on
         time.sleep_ms(1000)
         led.value(1)  # yellow LED off
